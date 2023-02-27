f1faabff1fb1f42eacd7cb2e08a1800760e09b24
Lucas <lucasmartinsms.dev@gmail.com>
2023-02-27 17:38:17 -0300

    login

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import autrouters from "../routes/autrouters.js";
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use();
+app.use(autrouters);
 
 
 
