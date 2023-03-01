f0cbd0edecdc70405c488ba00d9005d4f7bdcf7e
Lucas <lucasmartinsms.dev@gmail.com>
2023-03-01 17:07:37 -0300

    fix signup

--- src/schemas/uSchema.js
+++ src/schemas/uSchema.js
@@ -5,7 +5,7 @@ export const uSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().required(),
-  confirmpassword: joi.string().valid(joi.ref('password')).required()
+  confirmPassword: joi.string().valid(joi.ref('password')).required()
 });
 
 
