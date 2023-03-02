54bc9a49aab1cb78069c5f0fdca8cec5bf8aa49e
Lucas <lucasmartinsms.dev@gmail.com>
2023-03-02 13:53:44 -0300

    db

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,19 @@
-import pkg from 'pg';
-import dotenv from 'dotenv';
+
+import pg from "pg";
+import dotenv from "dotenv";
 dotenv.config();
 
+const { Pool } = pg;
 
-const { Pool } = pkg;
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+  ...(process.env.NODE_ENV === "production" && {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
+};
 
-	export const db = new Pool({
-        connectionString: process.env.DATABASE_URL,
-      });
+const db = new Pool(configDatabase);
+
+export default db;
