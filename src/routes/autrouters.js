ac9b82a4ddc72d3d116019d52b894e476f9e8b16
Lucas <lucasmartinsms.dev@gmail.com>
2023-03-02 13:40:54 -0300

    users-me

--- src/routes/autrouters.js
+++ src/routes/autrouters.js
@@ -2,13 +2,13 @@ import { Router } from "express";
 import { signUp, signIn } from "../controllers/autcontrollers.js";
 import {validateSchema } from "../middlewares/schemavalidate.js";
 import { uSchema,lSchema } from "../schemas/uSchema.js";
-import { validateToken } from "../middlewares/schemavalidate.js";
 import { getUser , getRanking} from "../controllers/usercontrollers.js";
+import { urlSchema } from "../schemas/urlSchema.js"
 
 const route = Router();
 route.post("/signup",validateSchema(uSchema), signUp);
 route.post("/signin",validateSchema(lSchema), signIn);
-route.get("/users/me", validateToken, getUser);
+route.get("/users/me", validateSchema(urlSchema), getUser);
 route.get("/ranking", getRanking);
 
 
