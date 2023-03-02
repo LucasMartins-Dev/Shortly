0fad987ded3b3e1dc8f67c68d7e194a2fdbffc47
Lucas <lucasmartinsms.dev@gmail.com>
2023-03-02 13:43:47 -0300

    shorten

--- src/routes/urlrouters.js
+++ src/routes/urlrouters.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { validateToken } from "../middlewares/schemavalidate.js";
+import { validateSchema, validateToken } from "../middlewares/schemavalidate.js";
 import { shorten, getUrlById, getShortUrl, deleteUrlById } from "../controllers/urlcontrollers.js";
 import { validateDelete , validateSchemaUrls } from "../middlewares/urlvalidate.js";
+import { urlSchema } from "../schemas/urlSchema.js";
 
 const route = Router();
 
 route.get("/urls/:id", getUrlById);
 route.get("/urls/open/:shortUrl", getShortUrl);
 route.use(validateToken);
-route.post("/urls/shorten", validateSchemaUrls, shorten);
+route.post("/urls/shorten", validateSchema(urlSchema), shorten);
 route.delete("/urls/:id", validateDelete,deleteUrlById);
 
 export default route;
