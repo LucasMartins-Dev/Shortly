1a2401a1456840005f5ece540194c763552c12e1
Lucas <lucasmartinsms.dev@gmail.com>
2023-03-02 13:19:27 -0300

    test

--- src/controllers/urlcontrollers.js
+++ src/controllers/urlcontrollers.js
@@ -1,21 +1,24 @@
 
-import {db}from '../database/database.js'
+import { db }from '../database/database.js'
 import { nanoid } from 'nanoid'
 
 export async function shorten(req, res) {
+    const { url } = req.body
     try {
-        const newUrl = res.locals.newUrl;
-        const { id, shortUrl, url } = newUrl;
-        await connectionDB.query(
-          `INSERT INTO urls ("shortUrl", url,"userId") VALUES ($1, $2, $3);`,
-          [shortUrl, url, id ]
-        );
-    
-        res.status(201).send(shortUrl);
-      } catch (err) {
-        console.log(err);
-        return res.sendStatus(500);
-      }
+        const session = res.locals.session;
+        const shortUrl = nanoid()
+        
+        await db.query(`
+            INSERT INTO urls ("shortUrl", url, "userId")
+            VALUES ($1, $2, $3);
+        `, [shortUrl, url, session.rows[0].userId])
+
+        const shortUrlId = await db.query(`SELECT id, "shortUrl" FROM urls WHERE "shortUrl"=$1;`, [shortUrl])
+
+        res.status(201).send(shortUrlId.rows[0])
+    } catch (error) {
+        res.status(500).send("Erro no servidor");
+    }
 }
 
 export async function getUrlById(req, res) {
