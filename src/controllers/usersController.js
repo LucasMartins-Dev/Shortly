7686485ae26dc5f51d53fc88c9ffc990c3d517d2
Lucas <lucasmartinsms.dev@gmail.com>
2023-03-02 18:26:55 -0300

    fix

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,30 +1,29 @@
 import { db } from "../database/database.js";
 
-
 export async function getUserLinks(req, res) {
-  const session = res.locals.session;
+  const session =  res.locals.session
 
   try {
     const links = await db.query(`
-      SELECT
-        users.id,
-        users.name,
-        COALESCE(SUM(urls.visitCount), 0) AS visitCount,
-        ARRAY_AGG(json_build_object(
-          'id', urls.id,
-          'shortUrl', urls.shortenUrl,
-          'url', urls.url,
-          'visitCount', urls.visitCount
-        )) AS shortenedUrls
-      FROM users
-      LEFT JOIN urls 
-        ON users.id = urls.userId
-      WHERE users.id = $1
-      GROUP BY users.id
-    `, [session.userId]);
+    SELECT
+      users.id,
+      users.name,
+      COALESCE(SUM(urls."visitCount"), 0) AS "visitCount",
+    ARRAY_AGG(json_build_object(
+      'id', urls.id,
+      'shortUrl', urls."shortenUrl",
+      'url', urls.url,
+      'visitCount', urls."visitCount"
+    )) AS "shortenedUrls"
+    FROM users
+    LEFT JOIN urls 
+      ON users.id = urls."userId"
+    WHERE users.id = $1
+    GROUP BY users.id
+    `, [session.userId])
 
-    res.status(200).send(links.rows[0]);
-  } catch (error) {
-    res.status(500).send(error.message);
+    res.send(links.rows[0])
+  } catch (err) {
+    res.status(500).send(err.message)
   }
 }
