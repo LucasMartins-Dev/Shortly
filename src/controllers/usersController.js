07483118dd574ae57de1b8efa559ea9fd2ea3ff4
Lucas <lucasmartinsms.dev@gmail.com>
2023-03-02 18:22:23 -0300

    fix usersme

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -23,7 +23,7 @@ export async function getUserLinks(req, res) {
       GROUP BY users.id
     `, [session.userId]);
 
-    res.send(links.rows[0]);
+    res.status(200).send(links.rows[0]);
   } catch (error) {
     res.status(500).send(error.message);
   }
